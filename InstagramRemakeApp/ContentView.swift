f3dd6325910baf622b87a99f2c232eff0dc88433
TheInventionMaker <nagswhiz@gmail.com>
2020-07-13 09:23:00 -0400

    Just a bottom Nav Bar for Instagram; we will change the text to a custom view in another class.

--- InstagramRemakeApp/ContentView.swift
+++ InstagramRemakeApp/ContentView.swift
@@ -9,15 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        ScrollView{
-            VStack{
-                Rectangle()
-                    .frame(width: 300, height: 400, alignment: .center)
-                    .foregroundColor(.gray)
-                .cornerRadius(40)
+        TabView {
+            Text("The content of the first view")
+              .tabItem {
+                 Image(systemName: "house.fill").font(.title)
+                 
+               }
+            Text("The content of the second view")
+              .tabItem {
+                 Image(systemName: "magnifyingglass").font(.title)
+                 
+               }
+                
+            Text("The content of the third view")
+            .tabItem {
+                Image(systemName: "plus.app").font(.title)
+               
+             }
+            Text("The content of the fourth view")
+            .tabItem {
+                Image(systemName: "heart").font(.title)
+                   
             }
-        }
+            Text("The content of the fifth view")
+            .tabItem {
+                Image(systemName: "person.circle").font(.title)
+                   
+            }
+        }.accentColor(Color.black)
     }
 }
 
